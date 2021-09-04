167e24380744bc172637186d9c2bd0db43e66cf1
Jonathan <jonathan.gomes@govbr.com.br>
2021-09-04 18:33:10 -0300

    first commit

---
+++ src/main/java/com/facebook/domain/UsuarioFacebook.java
@@ -0,0 +1,31 @@
+package com.facebook.domain;
+
+import java.time.LocalDate;
+
+public class UsuarioFacebook {
+    
+    private String nome;
+    
+    private String emprego;
+    
+    private String informacoesAcademicas;
+    
+    private String estadoCivil;
+    
+    private LocalDate dataNasc;
+    
+    private String data;
+    
+    private char sexo;
+    
+    private void criarUsuario() {
+        
+    UsuarioFacebook usuario1 = new UsuarioFacebook(); 
+    usuario1.nome = "Gustavito";
+    usuario1.dataNasc = LocalDate.of(1994, 10, 22);
+    usuario1.data = String.valueOf(dataNasc);
+
+    System.out.println(usuario1.nome + usuario1.data + usuario1.dataNasc);
+        
+    }
+}
